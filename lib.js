22501aab14c118652f731167e1484d41ef169628
Alex Michael Berry <alexmiberry@gmail.com>
2025-05-20 00:43:01 -0400

    adding editor for templates

--- lib.js
+++ lib.js
@@ -1,9 +1,30 @@
-import { runScript } from '@ligature/ligature-components';
+import { runScript, showEditor } from '@ligature/ligature-components';
 
 export function sisalInit() {
     document.querySelectorAll(".wander").forEach(el => {
-      const script = el.getAttribute("data-script")
+      const newElement = document.createElement("div")
+      const editorElement  = document.createElement("div")
+      editorElement.setAttribute("class", "editor")
+      const script = el.getAttribute("data-script").trim()
       el.setAttribute("data-script", "")
-      runScript(script, el)
+      el.append(editorElement)
+      el.append(newElement)
+      const runButton = document.createElement("button")
+      let editor = showEditor(editorElement, script)
+      runButton.addEventListener("click", e => {
+        newElement.innerHTML = ""
+        runScript(editor.state.doc.toString(), newElement)        
+      })
+      runButton.textContent = "Run"
+      el.append(runButton)
+      runScript(script, newElement)
     })
   }
+
+// export function sisalInit() {
+//     document.querySelectorAll(".wander").forEach(el => {
+//       const script = el.getAttribute("data-script")
+//       el.setAttribute("data-script", "")
+//       runScript(script, el)
+//     })
+//   }
