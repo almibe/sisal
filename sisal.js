850af1b902d4189462a65d00e16bb422db3faaba
Alex Michael Berry <alexmiberry@gmail.com>
2025-07-25 22:44:48 -0400

    removing old wander templates starting work on supporting wander data again

--- sisal.js
+++ sisal.js
@@ -1,41 +1,20 @@
-import { runAndGenerateHtml } from "@ligature/ligature";
-
-const isProduction = true
+import { run } from "@ligature/ligature";
 
 export default function sisalPlugin(eleventyConfig, pluginOptions) {
+  // eleventyConfig.addTemplateFormats("wander");
 
-  eleventyConfig.addTemplateFormats("wander");
-
-	eleventyConfig.addExtension("wander", {
-		compile: async (inputContent) => {
-
-      return async (data) => {
-        let res = runAndGenerateHtml(inputContent)
-        return res
-      };
-		},
-	});
-
-	// eleventyConfig.addExtension("wander-embed", {
+	// eleventyConfig.addExtension("wander", {
 	// 	compile: async (inputContent) => {
-	// 		return async () => {
-  //       return engine.render(embedTemplate, { script: inputContent })			
+
+  //     return async (data) => {
+  //       let res = runAndGenerateHtml(inputContent)
+  //       return res
   //     };
 	// 	},
 	// });
 
-
-  // eleventyConfig.addDataExtension("wander", (contents) => {
-  //   let res = run(contents)
-  //   return res
-  //   //return resultToJs(res)
-  // });
-  // eleventyConfig.addMarkdownHighlighter((str, lang) => {
-  //   if (lang != undefined && lang === "wander") {
-  //     let result = run(str)
-  //     return "<span class='code'>" + he.encode(str) + "</span>\n<hr>\nResult:\n" + he.encode(printResult(result))
-  //   } else {
-  //     return he.encode(str)
-  //   }
-  // })
+  eleventyConfig.addDataExtension("wander", (contents) => {
+    let res = run(contents)
+    return res
+  });
 }
